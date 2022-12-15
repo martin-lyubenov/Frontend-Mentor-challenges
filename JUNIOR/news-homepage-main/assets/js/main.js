0424f38533a90da01c809be9fbc6e0ceadbc5893
martin-lyubenov <52383146+martin-lyubenov@users.noreply.github.com>
2022-12-15 16:52:22 +0200

    Updating main index file

--- JUNIOR/news-homepage-main/assets/js/main.js
+++ JUNIOR/news-homepage-main/assets/js/main.js
@@ -1,9 +1,9 @@
 let image = document.getElementsByClassName('hero-img')[0]
 let width = document.querySelector('html').clientWidth
 if (width >= 700) {
-    image.setAttribute('src','/assets/images/image-web-3-desktop.jpg')
+    image.setAttribute('src','assets/images/image-web-3-desktop.jpg')
 } else { 
-    image.setAttribute('src','/assets/images/image-web-3-mobile.jpg')
+    image.setAttribute('src','assets/images/image-web-3-mobile.jpg')
 }
 
 const navContainer = document.querySelector('.nav-container');
