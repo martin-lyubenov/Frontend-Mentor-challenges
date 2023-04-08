fbc0db2f27f4dd736a6caf6ecade73d6a0a9a8e9
martin-lyubenov <52383146+martin-lyubenov@users.noreply.github.com>
2023-04-08 11:45:31 +0300

    Url adjustments

--- JUNIOR/news-homepage-main/assets/js/main.js
+++ JUNIOR/news-homepage-main/assets/js/main.js
@@ -1,10 +1,3 @@
-let image = document.getElementsByClassName('hero-img')[0]
-let width = document.querySelector('html').clientWidth
-if (width >= 700) {
-    image.setAttribute('src','assets/images/image-web-3-desktop.jpg')
-} else { 
-    image.setAttribute('src','assets/images/image-web-3-mobile.jpg')
-}
 
 const navContainer = document.querySelector('.nav-container');
 const mainNav = document.querySelector('.main-nav');
